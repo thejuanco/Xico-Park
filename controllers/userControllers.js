12f9eb44cea61d6dab960ccefa2e7c42fbe180d2
Juanco <juanffo874@gmail.com>
2023-06-13 20:05:30 -0600

    Añadiendo los formularios de registro, logueo y olvide mi contraseña, asi como sus rutas

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,6 +6,26 @@ const inicio = (req, res) =>{
     })
 }
 
+const register = (req, res) =>{
+    res.render('auth/register', {
+        
+    })
+}
+
+const formularioLogin = (req, res) =>{
+    res.render('auth/login', {
+        pagina: 'Login'
+    })
+}
+
+const forgotPassword = (req, res) =>{
+    res.render('auth/forgotPassword', {
+        pagina: 'Forgot Password'
+    })
+}
 export {
-    inicio
+    inicio, 
+    register,
+    formularioLogin, 
+    forgotPassword
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,15 @@
 //En este archivo se almacenaran las rutas exclusivamente del registro de usuarios 
 import express from 'express'
-import { inicio } from '../controllers/userControllers.js'
+import { inicio, formularioLogin, register, forgotPassword} from '../controllers/userControllers.js'
 
 const router = express.Router()
 
 router.get('/inicio', inicio)
+
+router.get('/login', formularioLogin)
+
+router.get('/register', register)
+
+router.get('/forgotPassword', forgotPassword)
 
 export default router
