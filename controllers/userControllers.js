2f6faab80608ac3ef62e017bedf33149e0c5223e
Juanco <juanffo874@gmail.com>
2023-06-17 10:19:31 -0600

    Agregando laas validaciones a los formularios de recuperar contraseña e iniciar sesion

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -40,7 +40,7 @@ const registrar = async (req, res) =>{
 
     res.render ('templates/message', {
         pagina: 'Cuenta creada correctamente', 
-        mensaje: `Hemos enviado un email de confirmacion, para el usuario: ${usuario.email}`, 
+        mensaje: `Hemos enviado un email de confirmacion, para el usuario: `, 
         email: req.body.correo
     })
 
@@ -54,15 +54,58 @@ const formularioLogin = (req, res) =>{
     })
 }
 
+const autenticar = async (req, res) =>{
+    //validar al usuario 
+    await check('correo').isEmail().withMessage('El correo no es valido').run(req)
+    await check('password').notEmpty().withMessage('La contraseña es obligatoria').run(req)
+
+    let resultado = validationResult(req) 
+
+    //Verificar que el resultado este vacio
+    if (!resultado.isEmpty()) {
+        return res.render('auth/login', {
+            pagina: 'Login', 
+            errores: resultado.array(), 
+        })
+    }
+
+    const {correo, password} = req.body
+    //comprobar si el usuario existe 
+    const usuario = await Usuario.findOne({where: {correo}})
+    if (!usuario){
+        return res.render('auth/login', {
+            pagina: 'Iniciar Sesion', 
+            errores: [{msg: 'El usuario no existe '}]
+        })
+    }
+}
+
 const forgotPassword = (req, res) =>{
     res.render('auth/forgotPassword', {
         pagina: 'Forgot Password'
     })
 }
+
+const resetPassword = async (req, res) =>{
+
+    await check('correo').isEmail().withMessage('El correo no es valido.').run(req)
+
+    let resultado = validationResult(req)
+
+    if (!resultado.isEmpty()) {
+            return res.render('auth/forgotPassword', {
+                pagina: 'Forgot Password', 
+                errores: resultado.array()
+            })
+        }
+}
+
 export {
     inicio, 
     formularioRegister, 
     registrar,
     formularioLogin, 
-    forgotPassword
+    autenticar,
+    forgotPassword, 
+    resetPassword
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,18 @@
 //En este archivo se almacenaran las rutas exclusivamente del registro de usuarios 
 import express from 'express'
-import { inicio, formularioLogin, formularioRegister, registrar, forgotPassword} from '../controllers/userControllers.js'
+import { inicio, formularioLogin, autenticar,formularioRegister, registrar, forgotPassword, resetPassword} from '../controllers/userControllers.js'
 
 const router = express.Router()
 
 router.get('/inicio', inicio)
 
 router.get('/login', formularioLogin)
+router.post('/login', autenticar)
 
 router.get('/register', formularioRegister)
 router.post('/register', registrar)
 
 router.get('/forgotPassword', forgotPassword)
+router.post('/forgotPassword', resetPassword)
 
 export default router
